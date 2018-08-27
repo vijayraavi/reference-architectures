b0de05035b793ed8ab3899e6c5b47bc284b78103
Nithin Madhavanpillai <nimadha@microsoft.com>
2018-08-27 10:50:45 -0700

    fixed folder structure

---
+++ data/streaming_azuredatabricks/azure/AzureDataBricksJob/src/main/scala/com/microsoft/pnp/TaxiRide.scala
@@ -0,0 +1,20 @@
+package com.microsoft.pnp
+
+case class TaxiRide(rateCode: Int,
+                    storeAndForwardFlag: String,
+                    dropoffTime: String,
+                    passengerCount: Int,
+                    tripTimeInSeconds: Double,
+                    tripDistanceInMiles: Double,
+                    pickupLon: Double,
+                    pickupLat: Double,
+                    dropoffLon: Double,
+                    dropoffLat: Double,
+                    medallion: Long,
+                    hackLicense: Long,
+                    vendorId: String,
+                    pickupTime: String)
+  extends TaxiData
+    with Serializable
+
+
