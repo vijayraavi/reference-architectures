78c6716ba5fb88eefe430411e4c74030c3db4996
Nithin Madhavanpillai <nimadha@microsoft.com>
2018-08-29 10:48:12 -0700

    added some checks for paremeters , eventhub for invalid records

--- data/streaming_azuredatabricks/azure/AzureDataBricksJob/build.sbt
+++ data/streaming_azuredatabricks/azure/AzureDataBricksJob/build.sbt
@@ -1,3 +1,5 @@
+
+
 name := "AzureDataBricksJob"
 
 version := "0.1"
@@ -16,9 +18,13 @@ lazy val AzureDataBricksJob = (project in file("."))
     libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.3.1" % "provided",
     libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.3.1",
     libraryDependencies += "com.microsoft.azure" % "azure-eventhubs-spark_2.11" % "2.3.1",
-    libraryDependencies += "org.geotools" % "gt-shapefile" % "19.2" from "http://download.osgeo.org/webdav/geotools/",
+    // https://mvnrepository.com/artifact/org.geotools/gt-shapefile
+    //    libraryDependencies += "org.geotools" % "gt-shapefile" % "20-RC",
+    // https://mvnrepository.com/artifact/commons-codec/commons-codec
+    libraryDependencies += "commons-codec" % "commons-codec" % "1.8",
     libraryDependencies += scalaTest % Test,
     dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-core" % "2.8.9",
     dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-databind" % "2.8.9",
     dependencyOverrides += "com.fasterxml.jackson.module" % "jackson-module-scala_2.11" % "2.8.9",
   )
+
--- data/streaming_azuredatabricks/azure/AzureDataBricksJob/src/main/scala/com/microsoft/pnp/TaxiCabReader.scala
+++ data/streaming_azuredatabricks/azure/AzureDataBricksJob/src/main/scala/com/microsoft/pnp/TaxiCabReader.scala
@@ -5,89 +5,125 @@ import java.sql.Timestamp
 
 import org.apache.spark.eventhubs.{ConnectionStringBuilder, EventHubsConf, EventPosition}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.streaming.{GroupState, GroupStateTimeout}
+import org.apache.spark.sql.streaming.{GroupState, GroupStateTimeout, Trigger}
+
+import scala.util.{Failure, Success, Try}
 
 object TaxiCabReader {
 
   val taxiRideEventHubName = "taxi-ride"
   val taxiFareEventHubName = "taxi-fare"
-  val sparkReadStreamFormatForEventHub = "eventhubs"
+  val sparkStreamFormatForEventHub = "eventhubs"
   val waterMarkTimeDuriation = "15 minutes"
   val maxThresholdBetweenStreams = "10 seconds"
+  val errorRecordsEventHubName = "taxi-ride" //TODO get one defined. time sbeing using an existing one
 
   def main(args: Array[String]) {
 
-    val spark = SparkSession.builder().config("spark.master", "local[10]").getOrCreate()
-    import spark.implicits._
 
-    // Taxi ride infra setup -
-    val rideEventHubSharedAccessKey = ""
+    Try(parseArgs(args)) match {
+      case Success(arguments) => {
 
-    val rideConnectionString = ConnectionStringBuilder(rideEventHubSharedAccessKey)
-      .setEventHubName(taxiRideEventHubName)
-      .build
+        val spark = SparkSession.builder().config("spark.master", "local[10]").getOrCreate()
+        import spark.implicits._
 
-    val rideEventHubConf = EventHubsConf(rideConnectionString)
-      .setStartingPosition(EventPosition.fromStartOfStream)
+        // Taxi ride infra setup -
 
-    //Taxi fare infra setup -
-    val fareEventHubSharedAccessKey = ""
+        val rideConnectionString = ConnectionStringBuilder(arguments.rideEventHubConnectionString)
+          .setEventHubName(taxiRideEventHubName)
+          .build
 
-    val fareConnectionString = ConnectionStringBuilder(fareEventHubSharedAccessKey)
-      .setEventHubName(taxiFareEventHubName)
-      .build
+        val rideEventHubConf = EventHubsConf(rideConnectionString)
+          .setStartingPosition(EventPosition.fromStartOfStream)
 
-    val fareEventHubConf = EventHubsConf(fareConnectionString)
-      .setStartingPosition(EventPosition.fromStartOfStream)
+        //Taxi fare infra setup -
 
+        val fareConnectionString = ConnectionStringBuilder(arguments.fareEventHubConnectionString)
+          .setEventHubName(taxiFareEventHubName)
+          .build
 
-    //Taxi ride read stream and extract taxi ride object -
-    val rideDataFrame = spark.readStream
-      .format(sparkReadStreamFormatForEventHub)
-      .options(rideEventHubConf.toMap)
-      .load
+        val fareEventHubConf = EventHubsConf(fareConnectionString)
+          .setStartingPosition(EventPosition.fromStartOfStream)
 
+        //Error records infra setup  -
 
-    val rides = rideDataFrame
-      .selectExpr("cast(body as string) AS rideContent",
-        "enqueuedTime As recordIngestedTime")
+        val errorRecordsEventHubConnectionString = ConnectionStringBuilder(arguments.errorRecordsEventHubConnectionString)
+          .setEventHubName(errorRecordsEventHubName)
+          .build
 
-
-    val taxiRideStream = rides.map(eventHubRow => TaxiRideMapper.mapRowToEncrichedTaxiRideRecord(eventHubRow))
-      .withWatermark("enqueueTime", waterMarkTimeDuriation)
-      .as[EnrichedTaxiDataRecord]
+        val errorEventHubConf = EventHubsConf(errorRecordsEventHubConnectionString)
+          .setStartingPosition(EventPosition.fromEndOfStream)
 
 
-    // Taxi fare read stream and extract fare object -
-    val fareDataFrame = spark.readStream
-      .format(sparkReadStreamFormatForEventHub)
-      .options(fareEventHubConf.toMap)
-      .load
-
-    val fares = fareDataFrame
-      .selectExpr("cast (body as string) AS fareContent",
-        "enqueuedTime As recordIngestedTime")
+        //Taxi ride read stream and extract taxi ride object -
+        val rideDataFrame = spark.readStream
+          .format(sparkStreamFormatForEventHub)
+          .options(rideEventHubConf.toMap)
+          .load
 
 
-    val taxiFareStream = fares.map(eventHubRow => TaxiFareMapper.mapRowToEncrichedTaxiFareRecord(eventHubRow))
-      .withWatermark("enqueueTime", waterMarkTimeDuriation)
-      .as[EnrichedTaxiDataRecord]
+        val rides = rideDataFrame
+          .selectExpr("cast(body as string) AS rideContent",
+            "enqueuedTime As recordIngestedTime")
 
 
-    val mergedTaxiDataRecords = taxiFareStream.union(taxiRideStream)
-
-    val invalidTaxiDataRecords = mergedTaxiDataRecords.filter(record => !record.isValidRecord)
-
-    val validTaxiDataRecords = mergedTaxiDataRecords.filter(record => record.isValidRecord)
+        val taxiRideStream = rides.map(eventHubRow => TaxiRideMapper.mapRowToEncrichedTaxiRideRecord(eventHubRow))
+          .withWatermark("enqueueTime", waterMarkTimeDuriation)
+          .as[EnrichedTaxiDataRecord]
 
 
-    val groupedEnrichedTaxiDataRecords = validTaxiDataRecords.groupByKey(_.taxiDataRecordKey)
-      .mapGroupsWithState(GroupStateTimeout.ProcessingTimeTimeout())(updateAcrossTaxiDataPerKeyRecords)
-      .filter(groupedRecordWithState => groupedRecordWithState.expired)
+        // Taxi fare read stream and extract fare object -
+        val fareDataFrame = spark.readStream
+          .format(sparkStreamFormatForEventHub)
+          .options(fareEventHubConf.toMap)
+          .load
 
-    groupedEnrichedTaxiDataRecords.writeStream.outputMode("update").format("console").start().awaitTermination()
+        val fares = fareDataFrame
+          .selectExpr("cast (body as string) AS fareContent",
+            "enqueuedTime As recordIngestedTime")
 
-    spark.stop
+
+        val taxiFareStream = fares.map(eventHubRow => TaxiFareMapper.mapRowToEncrichedTaxiFareRecord(eventHubRow))
+          .withWatermark("enqueueTime", waterMarkTimeDuriation)
+          .as[EnrichedTaxiDataRecord]
+
+
+        val mergedTaxiDataRecords = taxiFareStream.union(taxiRideStream)
+
+        val invalidTaxiDataRecords = mergedTaxiDataRecords.filter(record => !record.isValidRecord)
+
+
+        val validTaxiDataRecords = mergedTaxiDataRecords.filter(record => record.isValidRecord)
+
+
+        val groupedEnrichedTaxiDataRecords = validTaxiDataRecords.groupByKey(_.taxiDataRecordKey)
+          .mapGroupsWithState(GroupStateTimeout.ProcessingTimeTimeout())(updateAcrossTaxiDataPerKeyRecords)
+          .filter(groupedRecordWithState => groupedRecordWithState.expired)
+
+
+        invalidTaxiDataRecords
+          .writeStream
+          .format(sparkStreamFormatForEventHub)
+          .outputMode("update")
+          .options(errorEventHubConf.toMap)
+          .trigger(Trigger.ProcessingTime("25 seconds"))
+          .option("checkpointLocation", "./checkpoint/") // TODO paramterize checkpoint location and make it point to azure blob
+          .start()
+
+        groupedEnrichedTaxiDataRecords
+          .writeStream
+          .outputMode("update")
+          .format("console")
+          .start()
+          .awaitTermination()
+
+        spark.stop
+      }
+
+      case Failure(exception) => println(exception.getLocalizedMessage)
+    }
+
+
   }
 
 
@@ -99,6 +135,29 @@ object TaxiCabReader {
                                   var taxiRide: TaxiRide,
                                   var taxiFare: TaxiFare,
                                   var expired: Boolean)
+
+
+  case class Arguments(
+                        rideEventHubConnectionString: String,
+                        fareEventHubConnectionString: String,
+                        errorRecordsEventHubConnectionString: String
+                      )
+
+
+  def parseArgs(args: Array[String]): Arguments = {
+
+
+    if (args.length < 3) {
+      throw new Exception("please provide valid arguments for this program")
+    }
+
+    Arguments(
+      args(0),
+      args(1),
+      args(2)
+    )
+
+  }
 
 
   def updateTaxiDataPerKeyStateWithTaxiDataPerKeyRecord(state: TaxiDataPerKeyState, input: EnrichedTaxiDataRecord): TaxiDataPerKeyState = {
@@ -185,7 +244,7 @@ object TaxiCabReader {
         0,
         null,
         null,
-        false
+        expired = false
       )
 
       for (input <- inputs) {
