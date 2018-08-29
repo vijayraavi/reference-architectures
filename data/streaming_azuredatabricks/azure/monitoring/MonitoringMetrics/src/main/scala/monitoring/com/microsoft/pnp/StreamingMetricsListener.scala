720c26f315f518bbf51356214b61e2a3c40e56ef
cfarre <cfarre@microsoft.com>
2018-08-28 18:39:41 -0700

    BusinessWriter

---
+++ data/streaming_azuredatabricks/azure/monitoring/MonitoringMetrics/src/main/scala/monitoring/com/microsoft/pnp/StreamingMetricsListener.scala
@@ -0,0 +1,53 @@
+package monitoring.com.microsoft.pnp
+
+import org.apache.log4j._
+import org.apache.spark.sql.streaming.StreamingQueryListener
+import org.apache.spark.sql.streaming.StreamingQueryListener._
+import org.json4s._
+import org.json4s.native._
+import org.json4s.native.JsonMethods._
+import com.google.gson.Gson
+import com.github.ptvlogistics.log4jala._
+import java.util.HashMap
+import java.time.ZoneId
+import java.time.ZonedDateTime
+
+
+class StreamingMetricsListener() extends StreamingQueryListener {
+
+
+  implicit val formats = DefaultFormats
+  var logger:Logger = Logger.getLogger("Log4jALALogger")
+
+  override def onQueryStarted(event: QueryStartedEvent): Unit = {
+
+
+  }
+
+  override def onQueryProgress(event: QueryProgressEvent): Unit = {
+
+
+    try {
+      logger = Logger.getLogger("Log4jALALogger")
+      //parsing the telemetry Payload and logging to ala
+      logger.info(Utils.parsePayload(event))
+    }
+
+    catch {
+      case e: Exception => {
+        //parsing the error payload and logging to ala
+        logger = Logger.getLogger("Log4jALAError")
+        logger.error(Utils.parseError(e))
+      }
+    }
+
+  }
+
+  override def onQueryTerminated(event: QueryTerminatedEvent): Unit = {
+
+
+  }
+
+
+
+}
