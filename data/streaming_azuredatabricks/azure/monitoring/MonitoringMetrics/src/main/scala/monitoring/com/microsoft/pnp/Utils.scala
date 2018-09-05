2c87f1cecca8c1a70f6efb0fdc1235af3549b92f
cfarre <cfarre@microsoft.com>
2018-09-04 18:32:48 -0700

    added metrics for stream

--- data/streaming_azuredatabricks/azure/monitoring/MonitoringMetrics/src/main/scala/monitoring/com/microsoft/pnp/Utils.scala
+++ data/streaming_azuredatabricks/azure/monitoring/MonitoringMetrics/src/main/scala/monitoring/com/microsoft/pnp/Utils.scala
@@ -4,8 +4,6 @@ import java.time.{ZoneId, ZonedDateTime}
 import java.util.HashMap
 
 import org.apache.spark.sql.streaming.StreamingQueryListener.QueryProgressEvent
-import org.json4s.{DefaultFormats, JObject}
-import org.json4s.native.JsonMethods.parse
 
 import org.apache.spark._
 
@@ -24,45 +22,22 @@ import org.apache.log4j._
 
 object Utils {
 
-  implicit val formats = DefaultFormats
 
   def parsePayload(event: QueryProgressEvent): HashMap[String, AnyRef]={
     val date = java.time.format
       .DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneId.of("GMT")))
 
-    var dataJson = parse(event.progress.json).asInstanceOf[JObject]
-    var dataValue = dataJson \ "durationMs" \ "triggerExecution"
-    val triggerExecution: Double = dataValue.extract[Double]
-
-    dataValue = dataJson \ "durationMs" \ "getBatch"
-    val getBatch: Double = dataValue.extract[Double]
-
-    dataValue = dataJson \ "id"
-    val id: String = dataValue.extract[String]
-
-    dataValue = dataJson \ "numInputRows"
-    val inputRows: Integer = dataValue.extract[Integer]
-
-    dataValue = dataJson \ "sink" \ "description"
-    val sink: String = dataValue.extract[String]
-
-    dataValue = dataJson \ "inputRowsPerSecond"
-    val inRowsPerSecond: Double = dataValue.extract[Double]
-
-    dataValue = dataJson \ "processedRowsPerSecond"
-    val procRowsPerSecond: Double = dataValue.extract[Double]
-
     val metrics = new HashMap[String, AnyRef]()
 
-    metrics.put("name",event.progress.name)
-    metrics.put("id",  id)
-    metrics.put("sink",  sink)
-    metrics.put("triggerExecution",  triggerExecution.asInstanceOf[AnyRef])
-    metrics.put("getBatch", getBatch.asInstanceOf[AnyRef])
-    metrics.put("inputRowsPerSecond",  inRowsPerSecond.asInstanceOf[AnyRef])
-    metrics.put("procRowsPerSecond",  procRowsPerSecond.asInstanceOf[AnyRef])
-    metrics.put("inputRows",  inputRows.asInstanceOf[AnyRef])
+    metrics.put("id",  event.progress.id)
+    metrics.put("sink", event.progress.sink)
+    metrics.put("durationms",
+      event.progress.durationMs.asInstanceOf[AnyRef])
+    metrics.put("inputRowsPerSecond",  event.progress.inputRowsPerSecond.asInstanceOf[AnyRef])
+    metrics.put("procRowsPerSecond",  event.progress.processedRowsPerSecond.asInstanceOf[AnyRef])
+    metrics.put("inputRows",  event.progress.numInputRows.asInstanceOf[AnyRef])
     metrics.put("DateValue",date.toString())
+
 
     metrics
 
